5737667b54f25e8ac98010109dc24c4d395f6cc6
xiaotian <1512982931@qq.com>
2021-08-18 21:46:38 +0800

    second version

--- src/main/java/com/xiaotian/test.java
+++ src/main/java/com/xiaotian/test.java
@@ -3,5 +3,6 @@ package com.xiaotian;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("HELLO GIT");
     }
 }
